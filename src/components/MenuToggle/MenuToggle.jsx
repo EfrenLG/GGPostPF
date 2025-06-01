2e3b09bfb6c016d84597ee5cb724c997f67dd57a
EfrenLG <efrenlozanoguerrero@gmail.com>
2025-06-01 21:35:20 +0200

    boton menu prueba 4

--- src/components/MenuToggle/MenuToggle.jsx
+++ src/components/MenuToggle/MenuToggle.jsx
@@ -1,40 +1,24 @@
 import './MenuToggle.css';
 import { useNavigate } from 'react-router-dom';
-import { useRef } from 'react';
 
 const MenuToggle = () => {
 
     const navigate = useNavigate();
 
-    const checkboxRef = useRef(null);
-
-    const handleToggleClick = () => {
-        if (checkboxRef.current) {
-            checkboxRef.current.checked = !checkboxRef.current.checked;
-        }
-    };
-
-    const handleNavigate = (path) => {
-        if (checkboxRef.current) {
-            checkboxRef.current.checked = false; // cierra menú
-        }
-        navigate(path);
-    };
-
     return (
-        <div className="menu-toggle-container">
-            <input type="checkbox" id="menu-toggle" hidden ref={checkboxRef} />
-            <label htmlFor="menu-toggle" className="menu-button" onClick={handleToggleClick}>☰ Menú</label>
+        <div>
+            <input type="checkbox" id="menu-toggle" hidden />
+            <label htmlFor="menu-toggle" className="menu-button">☰ Menú</label>
             <label htmlFor="menu-toggle" className="overlay"></label>
             <aside className="side-menu">
                 <nav>
                     <ul>
-                        <li><a onClick={() => handleNavigate("/post")}>Publicaciones</a></li>
-                        <li><a onClick={() => handleNavigate("/rawgAPI")}>Los Mejores Valorados</a></li>
+                        <li><a onClick={() => navigate("/post")}>Publicaciones</a></li>
+                        <li><a onClick={() => navigate("/rawgAPI")}>Los Mejores Valorados</a></li>
                     </ul>
                 </nav>
             </aside>
-        </div >
+        </div>
     );
 };
 
