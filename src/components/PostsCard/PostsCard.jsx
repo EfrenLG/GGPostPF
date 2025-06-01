46f0a7421da4696dadf0e34794c7f0c65146add9
EfrenLG <efrenlozanoguerrero@gmail.com>
2025-06-02 00:43:48 +0200

    Posts prueba 3

--- src/components/PostsCard/PostsCard.jsx
+++ src/components/PostsCard/PostsCard.jsx
@@ -2,20 +2,19 @@ const PostsCard = (posts) => {
 
     return (
         <div className="userPosts">
-            {posts.map((post) => {
+            {posts.map((post) => (
                 <div className="post-card" key={post._id}>
                     <img
                         src={`https://ggpostb.onrender.com/post/${post.file}`}
                         alt={post.file}
-                        className="post-image">
-                    </img>
-
+                        className="post-image"
+                    />
                     <div className="post-details">
                         <h3>{post.tittle}</h3>
                         <p>{post.description}</p>
                     </div>
                 </div>
-            })}
+            ))}
         </div>
     );
 };
