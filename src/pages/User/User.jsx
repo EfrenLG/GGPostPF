21826e3069def7744b3cfb5cfc1c0a544209bcd3
EfrenLG <efrenlozanoguerrero@gmail.com>
2025-06-03 08:54:25 +0200

    User

--- src/pages/User/User.jsx
+++ src/pages/User/User.jsx
@@ -8,6 +8,7 @@ const User = () => {
 
     const [dataUser, setDataUser] = useState([]);
     const [dataPost, setDataPost] = useState([]);
+    const [selectedTab, setSelectedTab] = useState('datos');
     const navigate = useNavigate();
 
     const userId = localStorage.getItem('userId');
@@ -41,76 +42,98 @@ const User = () => {
     const icon = userIcon ? userIcon : 'default.png';
 
     return (
-        <>
-            <section className="user-panel">
-                <h2>Panel de Usuario</h2>
+        <section className="user-panel">
+            <h2>Panel de Usuario</h2>
 
-                <input type="radio" name="tab" id="tab-datos" checked />
-                <label htmlFor="tab-datos">Mis Datos</label>
+            <input
+                type="radio"
+                name="tab"
+                id="tab-datos"
+                checked={selectedTab === 'datos'}
+                onChange={() => setSelectedTab('datos')}
+            />
+            <label htmlFor="tab-datos">Mis Datos</label>
 
-                <input type="radio" name="tab" id="tab-user_posts" />
-                <label htmlFor="tab-user_posts">Mis Posts</label>
+            <input
+                type="radio"
+                name="tab"
+                id="tab-user_posts"
+                checked={selectedTab === 'user_posts'}
+                onChange={() => setSelectedTab('user_posts')}
+            />
+            <label htmlFor="tab-user_posts">Mis Posts</label>
 
-                <input type="radio" name="tab" id="tab-add_posts" />
-                <label htmlFor="tab-add_posts">Subir Post</label>
+            <input
+                type="radio"
+                name="tab"
+                id="tab-add_posts"
+                checked={selectedTab === 'add_posts'}
+                onChange={() => setSelectedTab('add_posts')}
+            />
+            <label htmlFor="tab-add_posts">Subir Post</label>
 
-                <input type="radio" name="tab" id="tab-close_session" />
-                <label htmlFor="tab-close_session">Cerrar Sesión</label>
+            <input
+                type="radio"
+                name="tab"
+                id="tab-close_session"
+                checked={selectedTab === 'close_session'}
+                onChange={() => setSelectedTab('close_session')}
+            />
+            <label htmlFor="tab-close_session">Cerrar Sesión</label>
 
-                <div className="tab-content datos">
-                    <form className="profile-form" id="profile-form">
-                        <div className="profile-image-wrapper">
-                            <input type="file" id="profile-pic" accept="image/*" />
-                            <label htmlFor="profile-pic" className="profile-image-label">
-                                <img src={`https://ggpostb.onrender.com/icons/${icon}`} alt="Imagen de perfil" id="changeIcon"
-                                    className="profile-image" />
-                            </label>
-                        </div>
+            <div className="tab-content datos">
+                <form className="profile-form" id="profile-form">
+                    <div className="profile-image-wrapper">
+                        <input type="file" id="profile-pic" accept="image/*" />
+                        <label htmlFor="profile-pic" className="profile-image-label">
+                            <img src={`https://ggpostb.onrender.com/icons/${icon}`} alt="Imagen de perfil" id="changeIcon"
+                                className="profile-image" />
+                        </label>
+                    </div>
 
-                        <div className="form-group">
-                            <label htmlFor={dataUser.username}>Nombre de usuario</label>
-                            <input type="text" id={dataUser.username} value={dataUser.username} />
-                        </div>
+                    <div className="form-group">
+                        <label htmlFor={dataUser.username}>Nombre de usuario</label>
+                        <input type="text" id={dataUser.username} value={dataUser.username} />
+                    </div>
 
-                        <div className="form-group">
-                            <label htmlFor={dataUser.email}>Correo electrónico</label>
-                            <input type="email" id={dataUser.email} value={dataUser.email} />
-                        </div>
+                    <div className="form-group">
+                        <label htmlFor={dataUser.email}>Correo electrónico</label>
+                        <input type="email" id={dataUser.email} value={dataUser.email} />
+                    </div>
 
-                        <button type="submit" className="btn-save-image">Guardar imagen</button>
-                    </form>
-                </div>
+                    <button type="submit" className="btn-save-image">Guardar imagen</button>
+                </form>
+            </div>
 
-                <div className="tab-content user_posts">
-                    <PostsCard posts={dataPost} />
-                </div>
+            <div className="tab-content user_posts">
+                <PostsCard posts={dataPost} />
+            </div>
 
-                <div className="tab-content add_posts">
-                    <form className="post-form" id="post-form">
-                        <div className="post-image-wrapper">
-                            <input type="file" id="post-pic" accept="image/*" />
-                            <label htmlFor="post-pic" className="post-image-label">
-                                <img src="" alt="Imagen del post" id="imagePost" className="post-image" />
-                            </label>
-                        </div>
+            <div className="tab-content add_posts">
+                <form className="post-form" id="post-form">
+                    <div className="post-image-wrapper">
+                        <input type="file" id="post-pic" accept="image/*" />
+                        <label htmlFor="post-pic" className="post-image-label">
+                            <img src="" alt="Imagen del post" id="imagePost" className="post-image" />
+                        </label>
+                    </div>
 
-                        <div className="form-group">
-                            <label htmlFor="tittle">Título del post</label>
-                            <input type="text" id="tittle" required />
-                            <p id="mess-titt"></p>
-                        </div>
+                    <div className="form-group">
+                        <label htmlFor="tittle">Título del post</label>
+                        <input type="text" id="tittle" required />
+                        <p id="mess-titt"></p>
+                    </div>
 
-                        <div className="form-group">
-                            <label htmlFor="description">Descripción del post</label>
-                            <input type="text" id="description" required />
-                            <p id="mess-des"></p>
-                        </div>
+                    <div className="form-group">
+                        <label htmlFor="description">Descripción del post</label>
+                        <input type="text" id="description" required />
+                        <p id="mess-des"></p>
+                    </div>
 
-                        <button type="submit" className="btn-save-post">Guardar post</button>
-                    </form>
-                </div>
-            </section>
-        </>
+                    <button type="submit" className="btn-save-post">Guardar post</button>
+                </form>
+            </div>
+        </section>
     );
 };
 
